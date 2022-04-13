0202592b47967b45b96fdceab76aad149cd48685
Eniyavan <eniyavan@Eniyavans-MacBook-Pro.local>
2022-04-13 12:02:39 +0530

    added version changes

--- app/src/main/java/com/plivo/plivosimplequickstart/PlivoBackEnd.java
+++ app/src/main/java/com/plivo/plivosimplequickstart/PlivoBackEnd.java
@@ -28,7 +28,8 @@ public class PlivoBackEnd implements EventListener {
 
         //Initiate SDK with Options, "enableTracking" (To get network related information)
         HashMap options = new HashMap();
-        options.put("maxAverageBitrate", 48000);
+//        options.put("maxAverageBitrate", 48000);
+//        options.put("enableTracking", true);
         endpoint = Endpoint.newInstance(context, log, this, options);
     }
 
